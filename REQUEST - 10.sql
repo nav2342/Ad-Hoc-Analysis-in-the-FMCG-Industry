643f71b3ffbc36fd94e11a937935ee4f0be75460
nav2342 <88432106+nav2342@users.noreply.github.com>
2025-02-07 08:40:30 +0530

    Update REQUEST - 10.sql

--- REQUEST - 10.sql
+++ REQUEST - 10.sql
@@ -23,7 +23,7 @@ GROUP BY division, product_code, product),
 	cte2 AS (
     SELECT
 		*,
-        ROW_NUMBER() OVER(PARTITION BY division ORDER BY total_sold_quantity DESC) AS rank_order
+        DENSE_RANK() OVER(PARTITION BY division ORDER BY total_sold_quantity DESC) AS rank_order
     FROM cte1)
 SELECT
 		division,
